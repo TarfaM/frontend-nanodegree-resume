6f8898641ad824a697070c0b5438188ea471a13c
tarfa <tarfa.mansour@gmail.com>
2017-08-13 21:11:42 +0300

    delete Hello World
    delete Hello World

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,11 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+$("#main").append(["Tarfa"]);
 
+var thought = "GoodDay";
+var newThought =thought.replace("Day","Morning");
+console.log(thought);
+console.log(newThought);
 
- console.log("gtfhht");
+$("#main").append(newThought);
